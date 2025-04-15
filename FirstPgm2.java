a0fb0ca6243eb6641d014b4ff0bf6d2ac727733d
Gayatri Kolhapure <kolhapuregayatri29@gmail.com>
2025-04-15 22:20:49 +0530

    Add files via upload

---
+++ FirstPgm2.java
@@ -0,0 +1,6 @@
+public class FirstPgm2 {
+    public static void main(String[] args) {
+        System.out.println("Hello, I'am Gayatri");
+        System.out.println("I'am pursuing b.Tech in Computer science engineering at Sharad Institute of technology college of engineering");
+    }
+}
