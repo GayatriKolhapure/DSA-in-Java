ec5cfaf15022648fbc46c1af7f3461a0a605476d
Gayatri Kolhapure <kolhapuregayatri29@gmail.com>
2025-04-15 21:07:59 +0530

    Add files via upload

---
+++ FirstPgm.java
@@ -0,0 +1,5 @@
+class FirstPgm {
+    public static void main(String args[]){//main function of the program our compiler starts to run the program from main function.
+        System.out.println("Hello World");//Print the text on console
+    }//end of the main function
+}
