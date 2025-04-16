a56e0d8bf45f9a472e02109c4e57cefb6255505a
Gayatri Kolhapure <kolhapuregayatri29@gmail.com>
2025-04-16 19:38:12 +0530

    Add files via upload

---
+++ secondPrg1.java
@@ -0,0 +1,17 @@
+public class secondPrg1 {
+    public static void main(String[] args) {
+        //Data types
+        short a = 5;
+        byte b = 5;//byte
+        char ch = 'a';//character
+        String name = "Gayatri";//String
+        int n = 23;//Integer
+        float f = 3.14f;//floating point value
+        double d = 0.3456d;//double 
+        System.out.println(a+"\n"+b+"\n"+ch+"\n"+name+"\n"+n+"\n"+f+"\n"+d);
+
+
+
+
+    }
+}
